e378be11369609093ddda96c3b0f2898ca647380
FabianRuizF <fabian.ruiz@alumnos.uach.cl>
2022-01-06 21:20:17 -0400

    Added bash script to change domain name on nginx
    Added script to facilitate the change of domain and docker container name on nginx conf and init-letsencrypt.sh

---
+++ change_domain_and_container.sh
@@ -0,0 +1,40 @@
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+    do
+        key="$1"
+        case $key in
+            -p|--docker_port)
+                DOCKER_PORT="$2"
+                shift
+                shift
+                ;;
+            -n|--docker_container_name)
+                DOCKER_CONTAINER_NAME="$2"
+                shift
+                shift
+                ;;
+            -d|--domain_name)
+                DOMAIN_NAME="$2"
+                shift
+                shift
+                ;;
+
+        esac
+    done
+
+
+
+
+
+
+
+new_domain=$DOMAIN_NAME
+ip=$(curl https://ipinfo.io/ip)
+sed -i 's/example.org/$new_domain/g' init-letsencrypt.sh
+sed -i 's/example.org/$new_domain/g' data/nginx/app.conf
+
+
+echo $ip
+echo $DOCKER_PORT
+echo $DOCKER_CONTAINER_NAME
+echo $DOMAIN_NAME
